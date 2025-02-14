923236c1172f4dfb5af238c84e888a8e4654f8ed
Visharad Mishra <visharadvidyasagar@gmail.com>
2025-02-14 21:32:24 +0530

    added global path

--- _app.py
+++ _app.py
@@ -3,7 +3,7 @@ from os import path
 
 #assests
 #SUN_VALLEY_THEME = path.join("venv\Lib\site-packages\sv_ttk\sv.tcl")
-SUN_VALLEY_THEME = path.join("SUNVALLEY\sv.tcl")
+SUN_VALLEY_THEME = path.join("SUNVALLEY","sv.tcl")
 
 JSDATA: dict
 
