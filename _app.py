4803bd26aee245d5f5a299fe11c1860ff25057da
Visharad Mishra <visharadvidyasagar@gmail.com>
2025-02-14 22:24:53 +0530

    Tried new theme variable

--- _app.py
+++ _app.py
@@ -4,6 +4,7 @@ from os import path
 #assests
 #SUN_VALLEY_THEME = path.join("venv\Lib\site-packages\sv_ttk\sv.tcl")
 SUN_VALLEY_THEME = path.join("SUNVALLEY","sv.tcl")
+THEME_MODE = path.join("SUNVALLEY","theme","light.tcl")
 
 APP_ICO = path.join("assets","app.ico")
 COFFEE_ICO = path.join("assets","coffe.ico")
